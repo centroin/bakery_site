46534a8b258d53fc96b75b8684b522fd7e24765a
centroin <user.03051998@gmail.com>
2020-02-06 00:04:19 +0530

    made gulp to watch the stylesheet

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,15 @@ gulp.task('default', function(){
 gulp.task('html', function(){
     console.log("jii woww");
 });
+gulp.task('styles', function(){
+    console.log("css works");
+});
 
 gulp.task('watch', function(){
     watch('./app/index.html',function(){
         gulp.start('html');
+    });
+    watch('./app/assets/styles/stylesheet.css',function(){
+        gulp.start('styles');
     });
 });
